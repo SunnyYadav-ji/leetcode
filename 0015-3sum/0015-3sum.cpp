91276af6df72f0023f6fbd7bc9dd4fd505eab14a
Sunny Kumar Yadav <220620584+SunnyYadav-ji@users.noreply.github.com>
2026-09-08 00:33:36 +0530

    Time: 42 ms (85.47%), Space: 29.1 MB (46.06%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,66 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-
+        int left, right, s, sum;
+        int n = nums.size();
         vector<vector<int>> result;
 
-        int n = nums.size();
-
-        // Step 1: Sort the array
         sort(nums.begin(), nums.end());
 
-        // Step 2: Fix one element
         for (int i = 0; i < n - 2; i++) {
-
-            // Skip duplicate first elements
-            if (i > 0 && nums[i] == nums[i - 1])
+            if (i > 0 and nums[i] == nums[i - 1])
                 continue;
-
-            // If the first element is positive,
-            // three positive numbers cannot sum to 0
-            if (nums[i] > 0)
-                break;
-
-            int left = i + 1;
-            int right = n - 1;
-
-            // Step 3: Two-pointer search
+            left = i + 1;
+            right = n - 1;
+            sum = -1 * nums[i];
             while (left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    result.push_back({
-                        nums[i],
-                        nums[left],
-                        nums[right]
-                    });
-
-                    // Skip duplicate second elements
-                    while (left < right &&
-                           nums[left] == nums[left + 1])
+                int s = nums[left] + nums[right];
+                if (s == sum) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    right--;
+                    left++;
+                    while (left < n and nums[left] == nums[left - 1])
                         left++;
-
-                    // Skip duplicate third elements
-                    while (left < right &&
-                           nums[right] == nums[right - 1])
+                    while (right >= 0 and nums[right] == nums[right + 1])
                         right--;
-
+                } else if (sum > s)
                     left++;
+                else
                     right--;
-                }
-                else if (sum < 0) {
-                    // Need a bigger sum
-                    left++;
-                }
-                else {
-                    // Need a smaller sum
-                    right--;
-                }
             }
         }
-
         return result;
     }
 };
